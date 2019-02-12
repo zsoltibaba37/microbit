51eff27ff69e520d80deeae45d60bbde9b29c72c
Zsolti <petozsolt33@gmail.com>
2019-02-12 11:19:41 +0100

    Simplify code. Add status led.

--- pwmV2.0.py
+++ pwmV2.0.py
@@ -3,65 +3,81 @@
 
 from microbit import *
 
-# init pwm value
+# Initial pwm value
 pwm = 500
-text = "PWM:50%"
+
+# Writes analog signal on the P0 leg
+pin0.write_analog(pwm)
+text = "PWM=50%"
 display.scroll(str(text), delay=100)
 display.clear()
 
+
+# Bar Graph function
 def bargraph(p):
     """
     :param p: internal pwm value
     """
-    BarValue = ((p * 25) // 1000)
-    BarRest = BarValue % 5
+    barvalue = (p * 20) // 1000
+    barrest = barvalue % 5
 
     if p <= 250:
-        for i in range(0, BarRest):
-            display.set_pixel(i, 4, 5)
-    elif p >= 250 and p < 500:
+        for i in range(0, barrest):
+            display.set_pixel(i, 4, 6)
+    elif 250 <= p < 500:
         for y in range(4, 3, -1):
             for x in range(0, 5):
-                display.set_pixel(4-x, y, 9)
-        for i in range(0, BarRest):
+                display.set_pixel(4 - x, y, 9)
+        for i in range(0, barrest):
             display.set_pixel(i, 0, 6)
-    elif p >= 500 and p < 750:
+    elif 500 <= p < 750:
         for y in range(4, 2, -1):
             for x in range(0, 5):
-                display.set_pixel(4-x, y, 9)
-        for i in range(0, BarRest):
+                display.set_pixel(4 - x, y, 9)
+        for i in range(0, barrest):
             display.set_pixel(i, 0, 6)
-    elif p >= 750 and p < 1000:
+    elif 750 <= p < 1000:
         for y in range(4, 1, -1):
             for x in range(0, 5):
                 display.set_pixel(4 - x, y, 9)
-        for i in range(0, BarRest):
+        for i in range(0, barrest):
             display.set_pixel(i, 0, 6)
-    elif p == 1000:
+    elif p >= 1000:
         for y in range(4, 0, -1):
             for x in range(0, 5):
                 display.set_pixel(4 - x, y, 9)
 
 
+# Status led BOOL
+s = False
+
+# Run forever
 while True:
     if button_a.get_presses():
         display.clear()
-        pwm -= 20
+        pwm -= 50
         if pwm <= 0:
             pwm = 0
-            pin0.write_analog(pwm)
         else:
             pin0.write_analog(pwm)
-
+        bargraph(pwm)
     if button_b.get_presses():
         display.clear()
-        pwm += 20
-        if pwm >= 1000:
-            pwm = 1000
-            pin0.write_analog(pwm)
+        pwm += 50
+        if pwm >= 1023:
+            pwm = 1023
         else:
             pin0.write_analog(pwm)
+        bargraph(pwm)
+    else:
+        bargraph(pwm)
 
-    bargraph(pwm)
-
-
+    # Status led in x=4 y=0 position
+    if s is False:
+        display.set_pixel(4, 0, 8)
+        sleep(pwm // 10)
+        s = True
+    elif s is True:
+        display.set_pixel(4, 0, 0)
+        sleep(pwm // 10)
+        s = False
